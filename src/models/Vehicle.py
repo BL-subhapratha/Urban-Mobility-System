e58254d069f6ff26f819bb1971e1023a08b79448
BL-subhapratha <subhapratha.sivaramakrishnan@bridgelabz.com>
2026-01-12 11:42:26 +0000

    feat:[Subha]-heirarchical inheritance of vehicle class

--- src/models/Vehicle.py
+++ src/models/Vehicle.py
@@ -17,3 +17,14 @@ class Vehicle:
         if not (0 < value < 100):
             print("Battery percentage must be between 0 and 100.")
         self.battery_percentage = value
+
+#UC3: Inheritance
+class ElectricCar (Vehicle):
+    def __init__(self, vehicle_id, model, battery_percentage, seating_capacity):
+        super().__init__(vehicle_id, model, battery_percentage)
+        self.seating_capacity = seating_capacity
+    
+class ElectricScooter(Vehicle):
+    def __init__(self, vehicle_id, model, battery_percentage, max_speed_limit):
+        super().__init__(vehicle_id, model, battery_percentage)
+        self.max_speed_limit = max_speed_limit
