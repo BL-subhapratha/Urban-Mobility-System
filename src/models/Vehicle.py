30f0fd85f0ad739c682b2f38a5efee4b617abdac
BL-subhapratha <subhapratha.sivaramakrishnan@bridgelabz.com>
2026-01-12 07:41:01 +0000

    feat:[Subha]-private attributes, getters and setters

--- src/models/Vehicle.py
+++ src/models/Vehicle.py
@@ -1,6 +1,19 @@
-#UC1: Basic Fleet Setup
+#UC1: define vehicle class and initialize attributes
 class Vehicle:
     def __init__(self, vehicle_id, model, battery_percentage):
         self.vehicle_id = vehicle_id
         self.model = model
         self.battery_percentage = battery_percentage
+        #UC2: private attribute
+        self.__maintenance_status = ''
+
+    #UC2: getters and setters
+    @property
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+    
+    @battery_percentage.setter
+    def battery_percentage(self, value):
+        if not (0 < value < 100):
+            print("Battery percentage must be between 0 and 100.")
+        self.battery_percentage = value
