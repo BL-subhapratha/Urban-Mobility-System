92fd57bcb793586c3e4ddde9183b4f4a4d93921a
BL-subhapratha <subhapratha.sivaramakrishnan@bridgelabz.com>
2026-01-13 05:18:31 +0000

    feat:[Subha]-added vehicle as ABC class

--- src/models/Vehicle.py
+++ src/models/Vehicle.py
@@ -1,5 +1,7 @@
+from abc import ABC, abstractmethod
+
 #UC1: define vehicle class and initialize attributes
-class Vehicle:
+class Vehicle(ABC):
     def __init__(self, vehicle_id, model, battery_percentage):
         self.vehicle_id = vehicle_id
         self.model = model
@@ -18,13 +20,25 @@ class Vehicle:
             print("Battery percentage must be between 0 and 100.")
         self.battery_percentage = value
 
+    #UC4: Abstraction
+    @abstractmethod
+    def calculate_trip_cost(self, distance):
+        return distance
+
 #UC3: Inheritance
 class ElectricCar (Vehicle):
     def __init__(self, vehicle_id, model, battery_percentage, seating_capacity):
         super().__init__(vehicle_id, model, battery_percentage)
         self.seating_capacity = seating_capacity
+
+    def calculate_trip_cost(self, distance):
+        pass
+    
     
 class ElectricScooter(Vehicle):
     def __init__(self, vehicle_id, model, battery_percentage, max_speed_limit):
         super().__init__(vehicle_id, model, battery_percentage)
         self.max_speed_limit = max_speed_limit
+
+    def calculate_trip_cost(self, distance):
+        pass
